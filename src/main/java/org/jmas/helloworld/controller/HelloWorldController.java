2b6bc45cf205e52c9312500483b857ef3b3ceb5b
jmas <jmas@qualityclouds.com>
2025-07-20 11:08:36 +0200

    ADD: Initial commit

---
+++ src/main/java/org/jmas/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package org.jmas.helloworld.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jmas5")
+public class HelloWorldController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        String message = "Hello World 222!";
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+}
